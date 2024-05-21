d3824d46fa227fb4a920d1fd4607d1b8924f2b29
Alex Grogan <agrogan097@gmail.com>
2024-05-21 11:58:50 +0100

    merged skeleton

---
+++ examples/skeleton/js/main.js
@@ -0,0 +1,21 @@
+// -- PARAMS --
+var assets = {"imgs" : {}, "fonts" : {}}
+var params = {verbose: false, positionMode: "PERCENTAGE", textAlign: "CENTER", imageMode: "CENTER", rectMode: "CENTER"};
+// ------------
+
+function handleClick(e){
+    // -- p5.js click listener -- //
+    pClickListener(e)
+}
+
+function preload(){
+
+}
+
+function setup(){
+
+}
+
+function draw(){
+    clear();
+}

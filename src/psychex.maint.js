9e67905229e017a0ee4295f4ceed066c498f9a53
Alex Grogan <agrogan097@gmail.com>
2024-10-10 16:14:59 +0100

    start_kit edits and express template

--- src/psychex.maint.js
+++ src/psychex.maint.js
@@ -1399,7 +1399,7 @@ class Game {
      * @param {string} url=`api/save/`: The endpoint URL that the data will be saved to, such as the address on an external server.
      * @returns {Promise} The promise of a response from the target endpoint.
      */
-    async saveDataToServer(data, url=`api/save/`){
+    async saveDataToServer(data, url=`api/save`){
         const URL = url;
         const response = await fetch(URL, {
             method: 'POST',
@@ -1419,7 +1419,7 @@ class Game {
      * @param {any} data: Additional parameters to pass to the endpoint
      * @returns {Promise}
      */
-    async loadDataFromServer(url=`api/load/`, data={}){
+    async loadDataFromServer(url=`api/load`, data={}){
         const URL = url;
         const response = await fetch(URL, {
             method: 'GET',
@@ -1741,6 +1741,26 @@ class Utils{
             })
             return vals
         }
+    }
+
+    /**
+     * Return a boolean indicating if the device is a touch screen device (mobile or tablet).
+     * Courtesy of detectmobilebrowsers.com
+     * NB: Parsing device type from user agent is not the most stable process, so worth testing before uses.
+     * @returns {boolean} true if touch device (mobile or tablet), else false
+     */
+    static isTouchDevice(){
+        let check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    }
+
+    /**
+     * Detect if the device is in portrait or landscape mode. If in landscape return true, if not return false.
+     * @returns {boolean} return true if device is in landscape, false if not
+     */
+    static isLandscape(){
+        return screen.orientation.type.includes("landscape");
     }
 }
 
@@ -2177,7 +2197,7 @@ class GridWorld extends Primitive {
         // Be either keyboard or click-controllable
         // Offer callbacks for what happens before movement, and what happens after movement
         // mode is either 'arrows', 'wasd', 'click'
-        if (!["arrows", "wasd", "click"].includes(mode)){throw new Error(`Mode type ${mode} not recognised. Must be one of: arrows, wasd, click.`)};
+        if (!["arrows", "wasd", "click", "buttons"].includes(mode)){throw new Error(`Mode type ${mode} not recognised. Must be one of: arrows, wasd, click.`)};
 
         // Define a function that calls the first preMovement callback, and if that returns true, call the post movement callback
         // For example, the preMovementcallback might contain rules about wall placements, etc.
@@ -2204,6 +2224,16 @@ class GridWorld extends Primitive {
                 cell.obj.toggleClickable();
                 cell.obj.onClick = callback;
             })
+        } else if (mode == "buttons"){
+            // Parse direction from key
+            const keys = ["up", "down", "left", "right"];
+            // Look for a buttons object and attach the callback to each button
+            if (this.buttons == undefined){
+                console.log("No object named this.buttons found - to automatically attach the callback to buttons, name the object this.buttons. This function will now return the callback for you to manually attach.")
+                return callback;
+            } else {
+                Object.values(this.buttons).forEach(b => {b.onClick = callback});
+            }
         }
     }
 
@@ -2225,7 +2255,11 @@ class GridWorld extends Primitive {
             'a' : [0, -1],
             'd' : [0, 1],
             'w' : [-1, 0],
-            's' : [1, 0]
+            's' : [1, 0],
+            'left' : [0, -1],
+            'right' : [0, 1],
+            'up' : [-1, 0],
+            'down' : [1, 0]
         }
         
         let newPos = [pos[0] + keyMapping[k][0], pos[1] + keyMapping[k][1]];
